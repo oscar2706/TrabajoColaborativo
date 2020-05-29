e3164195859db87249c1da3e8e2b5cd8e7ecba1e
Dylan Renalty <pywiii115@gmail.com>
2020-05-29 16:58:19 -0500

    coincidencia de contraseñas

--- profesor/Registro.php
+++ profesor/Registro.php
@@ -2,6 +2,15 @@
 include 'Conexion.php';
     $Correo = $_POST["Correo"];
     $Password = $_POST["Password"];
+    $ConfirmaPassword = $_POST["ConfirmaPassword"];
+ 
+
+    if ($Password == $ConfirmaPassword)
+    {
+        echo 'correcto';
+    }else {
+      echo 'incorrecto corrige wey';
+    }
 
     $insetar = "INSERT INTO profesor (Correo, Password) VALUES ('$Correo', '$Password')";
     $resultado = mysqli_query($conexion, $insetar);
